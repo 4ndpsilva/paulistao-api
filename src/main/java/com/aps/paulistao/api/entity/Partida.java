a3a27f984cc6e8b7161858d29a7d7687ad0f56aa
André <4ndpsilva@gmail.com>
2021-03-28 05:40:17 -0300

    Adição de configurações de pool de conexões usando Hikari

--- src/main/java/com/aps/paulistao/api/entity/Partida.java
+++ src/main/java/com/aps/paulistao/api/entity/Partida.java
@@ -13,7 +13,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -22,7 +21,6 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @Entity
 @Table(name = "TB_PARTIDA")
