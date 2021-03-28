60c1534f8b0adbe3df879f16d3e3abb18705cdd0
André P. Silva <4ndpsilva@gmail.com>
2021-03-28 01:04:32 -0300

    Web scraping de partidas - fase inicial
    * Refatoração geral de código
    
    * Transformação de classe em serviço

---
+++ src/test/java/com/aps/paulistao/api/PaulistaoAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.paulistao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaulistaoAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
